79d2f158ab66233b1c256efaedeea5ecb31154d5
Pavel Safronov <pv.safronov@gmail.com>
2025-11-16 11:47:22 +0000

    dont create pidfile as it breaks on container restart

--- run-chrome.sh
+++ run-chrome.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -uex
+
+
 socat TCP-LISTEN:9222,fork,reuseaddr,bind=0.0.0.0 TCP:127.0.0.1:59222 & 
 Xvfb :1 -screen 0 1024x768x16 -ac -nolisten tcp -nolisten unix & 
 DISPLAY=:1 fluxbox & 
 DISPLAY=:1 x11vnc -nopw -forever -localhost -shared -rfbport 5900 -rfbportv6 5900 & 
 DISPLAY=:1 websockify -D --web=/usr/share/novnc 7900 localhost:5900 & 
 
-dbus-daemon --system --fork --print-address 1 > /tmp/dbus-session-addr.txt
+dbus-daemon --nopidfile --nosyslog --system --fork --print-address 1 > /tmp/dbus-session-addr.txt
 
 export DBUS_SESSION_BUS_ADDRESS=$(cat /tmp/dbus-session-addr.txt)
 
